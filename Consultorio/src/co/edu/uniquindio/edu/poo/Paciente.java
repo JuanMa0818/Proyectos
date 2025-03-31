3ff56f7cdc6f3359df8af848e36d4d6dd6ceb20b
ASUS <ASUS@DESKTOP-AK4T53C>
2025-03-30 23:35:34 -0500

    COMMIT

---
+++ Consultorio/src/co/edu/uniquindio/edu/poo/Paciente.java
@@ -0,0 +1,10 @@
+package co.edu.uniquindio.edu.poo;
+
+import java.time.LocalDate;
+
+public record Paciente (String nombre, String apellido, String identificacion, String telefono, LocalDate ultimaFecha, int cantidadTratamientos) 		{
+
+	
+	}
+
+
