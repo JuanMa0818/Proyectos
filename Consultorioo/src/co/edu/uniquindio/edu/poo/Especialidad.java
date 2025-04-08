90130c0218a82ef9c8a0a850c1b9f48347d5699f
JuanMa0818 <juanm.mejiap1@uqvirtual.edu.co>
2025-04-08 16:25:46 -0500

    Add files via upload

---
+++ Consultorioo/src/co/edu/uniquindio/edu/poo/Especialidad.java
@@ -0,0 +1,11 @@
+package co.edu.uniquindio.edu.poo;
+
+public enum Especialidad {
+	
+	ODONTOLOGIA_GENERAL,
+	ORTODONCIA,
+	ENDODONCIA,
+	CIRUGIA_ORAL
+	
+
+}
---
+++ Consultorioo/src/co/edu/uniquindio/edu/poo/Odontologo.java
@@ -0,0 +1,6 @@
+package co.edu.uniquindio.edu.poo;
+
+public record Odontologo (String nombre, String apellidos, String identificacion, String numeroLicencia, Especialidad especialidad) {
+
+
+}
