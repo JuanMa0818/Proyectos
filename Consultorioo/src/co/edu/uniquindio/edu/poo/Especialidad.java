04622894601060fc83543f8af92034003939c7f8
JuanMa0818 <juanm.mejiap1@uqvirtual.edu.co>
2025-10-17 12:49:50 -0500

    Update Especialidad.java

--- Consultorioo/src/co/edu/uniquindio/edu/poo/Especialidad.java
+++ Consultorioo/src/co/edu/uniquindio/edu/poo/Especialidad.java
@@ -1,11 +1,45 @@
-package co.edu.uniquindio.edu.poo;
+package org.uniquindio.edu.co.poo.parcial02.model;
 
-public enum Especialidad {
-	
-	ODONTOLOGIA_GENERAL,
-	ORTODONCIA,
-	ENDODONCIA,
-	CIRUGIA_ORAL
-	
+import org.uniquindio.edu.co.poo.parcial02.controller.Tablacontroller;
 
+public class SistemaInmobiliarioFacade {
+    private static SistemaInmobiliarioFacade instance;
+
+    private SistemaInmobiliarioFacade() {
+    }
+
+    public static SistemaInmobiliarioFacade getInstance() {
+        if (instance == null) {
+            instance = new SistemaInmobiliarioFacade();
+        }
+        return instance;
+    }
+
+    public boolean registrarInmueble(String tipo, double precio, String ciudad,
+                                     int numHabitaciones, int numPisos) {
+        try {
+            Inmueble inmueble = crearInmueble(tipo, precio, ciudad, numHabitaciones, numPisos);
+            Tablacontroller.agregarInmuebleStatic(inmueble);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private Inmueble crearInmueble(String tipo, double precio, String ciudad,
+                                   int numHabitaciones, int numPisos) {
+        switch (tipo) {
+            case "CASA":
+                return new Casa(precio, ciudad, numHabitaciones, numPisos);
+            case "APARTAMENTO":
+                return new Apartamento(precio, ciudad, numHabitaciones, numPisos);
+            case "FINCA":
+                return new Finca(precio, ciudad, numHabitaciones, numPisos);
+            case "LOCAL":
+                return new Local(precio, ciudad, numHabitaciones, numPisos);
+            default:
+                throw new IllegalArgumentException("Tipo de inmueble no válido");
+        }
+    }
 }
